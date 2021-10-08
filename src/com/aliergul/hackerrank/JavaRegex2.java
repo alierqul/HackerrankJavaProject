0f133486179597a8480c85d44c72fb57bbd8dd25
alier <alier@ALIERQUL>
2021-10-08 23:18:15 +0300

    JavaRegex2

---
+++ src/com/aliergul/hackerrank/JavaRegex2.java
@@ -0,0 +1,32 @@
+package com.aliergul.hackerrank;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class JavaRegex2 {
+	private static final int count = 5;
+	private static String[] test = { "Goodbye bye bye world world world", "Sam went went to to to his business",
+			"Reya is is the the best player in eye eye game", "in inthe", "Hello hello Ab aB" };
+
+	public static void main(String[] args) {
+
+		String pattern = "\\b(\\w+)(\\s+\\1\\b)*";
+		Pattern p = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+		for (int i = 0; i < count; i++) {
+			Matcher m = p.matcher(test[i]);
+			try {
+
+				// Check for subsequences of input that match the compiled pattern
+				while (m.find()) {
+					test[i] = test[i].replaceAll(m.group(0), m.group(1));
+
+				}
+			} catch (PatternSyntaxException e) {
+				System.out.println("Invalid");
+			}
+			System.out.println(test[i]);
+		}
+
+	}
+}
